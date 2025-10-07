fe2988a0285811ec5cb7fba35f6dfa96bbddaec5
milenamarkovich <43650087+milenamarkovich@users.noreply.github.com>
2025-10-07 14:53:03 -0400

    Function exercises

---
+++ excersises_10.7.2025.py
@@ -0,0 +1,47 @@
+'''
+Python Functions - Inputs, Outputs, Local vs. Global Variables
+'''
+
+'''
+Exercise 1) Create a function with an arbitrary number of arguments. Print the functions arguments when you call your function.
+'''
+# Your code goes here!
+
+'''
+Exercise 2) Create a function that returns two values: the sum and the subtraction of 2 integers.
+'''
+# Your code goes here!
+
+'''
+Exercise 3) Create a function that has a default variable. For example, we want to print a student's name
+and their grade. Set the default grade to 85.
+'''
+# Your code goes here!
+
+
+'''
+Exercise 4) Create a function within a function! Define an outer function to accept 2 parameters: a and b.
+Then define an inner function which sums together parameters a & b. Add 5 to the result, and return final value
+in your outer function.
+'''
+# Your code goes here!
+
+'''
+Exercise 5) Create a recursive function! A recursive function is a function that recurrently
+calls on itself in a loop, until some breakout condition is satisfied. 
+Define a function that calculates the factorial of an integer, n (i.e. n!). 
+For example, if n=5, we want to calculate 5! = 5 * 4 * 3 * 2 * 1.
+'''
+# Your code goes here!
+
+'''
+Exercise 6) Create a function that accepts any keyword arguments and prints
+the key-value pairs
+'''
+# Your code goes here!
+
+'''
+Exercise 7) Define a global variable and set it equal to 10. Then create a function which modifies
+the value of this global variable.
+'''
+# Your code goes here!
